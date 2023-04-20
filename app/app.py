e33f168cd46bd7dff3de9947f65ce1dfca6b0cd5
Yamir Ramirez <ysramirez@gmail.com>
2023-04-20 01:58:00 +0000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -11,7 +11,7 @@ def mainpage():
 
 	key = default_key
 	if 'key' in request.form:
---	    key = request.form['key']
+        	key = request.form['key']
 
 	if request.method == 'POST' and request.form['submit'] == 'save':
 		cache.set(key, request.form['cache_value'])
